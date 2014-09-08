af3dc778e00ad073dc017e7478192df879ad0608
Mateus Zitelli <zitellimateus@gmail.com>
2014-09-08 01:01:21 -0300

    Add autodeploy of docs to gh-page

---
+++ deploy-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf out || exit 0;
+gulp buildDocs
+( cd docs
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
