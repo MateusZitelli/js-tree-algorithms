666cead22faba57400e82a9e7428440515c0fde6
Mateus Zitelli <zitellimateus@gmail.com>
2014-09-07 23:20:51 -0300

    Add taks for docs generation using docco
    Colses #4

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var docco = require("gulp-docco");
+
+gulp.task('buildDocs', function() {
+  return gulp.src('./src/**/*.js')
+    .pipe(docco())
+    .pipe(gulp.dest('docs'));
+});
 
 gulp.task('test', function () {
   return gulp.src('tests/**/*.js', {read: false})
