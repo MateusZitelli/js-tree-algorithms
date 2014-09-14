eeaa0446f6e1fbf62afacd6af51893bbcd5a4925
Travis-CI <travis@nodemeatspace.com>
2014-09-14 16:57:45 -0300

    Fix script name on .travis.yml

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -8,6 +8,7 @@ git config user.name "Travis-CI"
 git config user.email "travis@nodemeatspace.com"
 git add .
 if git commit -m "Deployed to Github Pages"; then
+  echo "https://${GH_TOKEN}@${GH_REF}"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 else
   exit 0;
